c628611664e43a7057663730467845972f25efd3
yoonhyungkim <yh960909@gmail.com>
2020-08-09 11:17:48 -0700

    Simple NN using python during ETRI

---
+++ NN.py
@@ -0,0 +1,72 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+def sigmoidFunction(z, derivative=False):
+    """deriavative: get normal when false,
+    while derivative when true"""
+    if derivative: return z*(1.0-z)
+    else: return 1.0/(1.0+np.exp(-z))
+
+
+def lossFunction(Out,Desired):
+    """Out:result of forward
+    Desired: desired Result
+    Calculated Sum of Sqauared Error"""
+    return ((Desired-Out)**2).sum()     #Guess this work as sigma
+
+
+class NeuralNetwork:
+    def __init__(self,n_x,n_h,n_y):
+        """n_x: number of input nodes
+        n_h: number of hidden nodes
+        n_y: number of output noded
+        node: num of data for single layer"""
+        self.W1=np.random.rand(n_x,n_h)
+        self.W2=np.random.rand(n_h,n_y)
+        self.hidden=np.zeros((1,n_h))       #(row,col)=>interpretate each low has 1 data and each col has n_h data
+        self.output=np.zeros((n_y,1))
+        self.activation=sigmoidFunction
+
+    def feedforward(self,In):
+        """In: input data"""
+        self.hidden=self.activation(np.dot(In,self.W1)) # (1,n_x)*(n_X,n_h)=(1,n_h)
+        self.output=self.activation(np.dot(self.hidden,self.W2))
+        return self.output
+
+    def backprop(self,In,Out,Desired):
+        """In:input data
+        Out:the result of forward propagation
+        Desired: desired value
+        application of the chain rule to find derivative of the loss function with respect to W2 and W1"""
+        diff=Out-Desired
+        d_W2=np.dot(self.hidden.T,(2*diff*self.activation(Out,True)))
+        d_W1=np.dot(In.T,\
+                np.dot(2*diff*self.activation(Out,True),self.W2.T)*self.activation(self.hidden,True))
+        self.W1-=d_W1
+        self.W2-=d_W2
+
+
+if __name__=="__main__":
+    X=np.array([[0,0,1],
+        [0,1,1],
+        [1,0,1],
+        [1,1,1]])
+    Y=np.array([[0],[1],[1],[0]])
+
+    nn=NeuralNetwork(X.shape[1],4,Y.shape[1])   #unlike verilog python index start from front->back (3,4,1)  //(4,3), (1,1) (1,4)
+    
+    loss_values=[]
+    for i in range(1000):
+        z=nn.feedforward(X)
+        nn.backprop(X,z,Y)
+        loss=lossFunction(z,Y)
+        loss_values.append(loss)
+
+
+    print(nn.output)
+
+    plt.plot(loss_values)
+    plt.xlabel("Iterations"); plt.xlim(-10,len(loss_values))
+    plt.ylabel("Loss")
+    plt.show()
+
